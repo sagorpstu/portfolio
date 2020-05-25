a810fe57d0ee22db2057c923d1c942afe4380a97
F.H. SAGOR <sagorpstu17@gmail.com>
2020-05-25 15:34:07 +0600

    Update active.js

--- js/active.js
+++ js/active.js
@@ -7,31 +7,4 @@ $(document).ready(function () {
     new WOW().init();
     
     // ?Wow Animation
-
-     $("body").niceScroll({
-     cursorcolor: "#2ecc71",
-     cursorwidth: "12px",
-     cursorborder: "0px solid #000",
-     scrollspeed: 60,
-     autohidemode: true,
-     background: "#ddd",
-     hidecursordelay: 400,
-     cursorfixedheight: false,
-     cursorminheight: 20,
-     enablekeyboard: true,
-     horizrailenabled: true,
-     bouncescroll: false,
-     smoothscroll: true,
-     iframeautoresize: true,
-     touchbehavior: false,
-     zindex: 999,
-   });
-
-    
-
-
-
-
-
-
 });
