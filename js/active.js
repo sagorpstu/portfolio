6d578932d7cef9ac811af9564c6e2f7fc4d60f11
F.H. SAGOR <sagorpstu17@gmail.com>
2020-05-25 15:01:56 +0600

    Update active.js

--- js/active.js
+++ js/active.js
@@ -8,7 +8,24 @@ $(document).ready(function () {
     
     // ?Wow Animation
 
-    // ?Wavify
+     $("body").niceScroll({
+     cursorcolor: "#2ecc71",
+     cursorwidth: "12px",
+     cursorborder: "0px solid #000",
+     scrollspeed: 60,
+     autohidemode: true,
+     background: "#ddd",
+     hidecursordelay: 400,
+     cursorfixedheight: false,
+     cursorminheight: 20,
+     enablekeyboard: true,
+     horizrailenabled: true,
+     bouncescroll: false,
+     smoothscroll: true,
+     iframeautoresize: true,
+     touchbehavior: false,
+     zindex: 999,
+   });
 
     
 
